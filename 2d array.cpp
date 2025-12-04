72c19b78c8a7ef5b665b4f6e97adda4b41723a13
sree21060-commits <sree21060@gmail.com>
2025-12-04 13:50:40 +0530

    Add files via upload

---
+++ 2d array.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][3],i,j;
+	printf("Enter array elements");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[1][3]);
+		}
+	}
+	printf("array denotes arc");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d",a[1][3]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
